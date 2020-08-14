330a7dce52256e31d94ce563bb89514a4e4c5733
Marr11317 <remimarche@gmail.com>
2020-08-13 23:07:02 -0400

    prettify output

--- index.ts
+++ index.ts
@@ -1,10 +1,33 @@
-require { Renderer } from 'renderer';
-
 // Import stylesheets
 import './style.css';
+import { Renderer } from './renderer';
 
 const chords = ['C13', 'FMaj7#11', 'G-7', 'C7(b9)', 'BMaj9#11', 'Bb13', 'A7alt', 'D-11', 'G7(b13)', 'CMaj7']
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+const rend = new Renderer(chords);
+
+function render(){
+  const appDiv: HTMLElement = document.getElementById('app');
+  let h = rend.toString();
+    // Collapse and normalize breaks
+  h = h.replace( /[\t ]*(<br *\/?>|\r|\n)+/gi, '\n\t' );
+    // Remove leading and trailing breaks
+	h = h.replace( /((\n\t)+$())|(^(\n\t)+([^\n]))/g, '$6' );
+    // Replace breaks with table rows
+	h = h.replace( /(\n)/g, '</tr><tr>' );
+    // Replace tabs with cells
+	h = h.replace( /(\t+)([^\t]+)/g, '<td>$2</td>' );
+    // Wrap it all up
+	appDiv.innerHTML = '<table class="stops">' + h + '</table>';
+}
+
+document.getElementById('up').addEventListener("click", function(){
+  rend.transpose += 1;
+  render();
+});
+document.getElementById('down').addEventListener("click", function(){
+  rend.transpose -= 1;
+  render();
+});
+
+render();
--- renderer.ts
+++ renderer.ts
@@ -1,24 +1,26 @@
-require { parseChord, chordRendererFactory } from 'chord-symbol';
+import { parseChord, chordRendererFactory } from 'chord-symbol';
 
 export class Renderer {
   chords: Array<string> = [];
+  transpose: number = 0;
 
   constructor(chords){
-
+    this.chords = chords
   }
 
-  print(): string{
-    let result = '|';
-    this.chords.forEach((chord: string) => {
-      const c = parseChord(chord);
-      const renderC = chordRendererFactory({ 
-        useShortNamings: true,
-        simplify: 'core',
-        transposeValue: 0,
-        harmonizeAccidentals: false,
-        useFlats: true
-      })
-      result += renderC + '|';
+  toString(): string{
+    let result = '';
+    const renderC = chordRendererFactory({ 
+      useShortNamings: false,
+      simplify: 'none',
+      transposeValue: this.transpose,
+      harmonizeAccidentals: false,
+      useFlats: true
+    });
+    this.chords.forEach((chord: string, index: number) => {
+      if (!(index % 4))
+        result += '\n|'
+      result += renderC(parseChord(chord)) + '\t|';
     })
 
     return result;
